ff476ecbf6e469d1f09707cca5fcfabbfae4bf64
enue <nnp@car.ocn.ne.jp>
2018-05-07 13:42:30 +0900

    fix JsonToExcel

--- JsonToExcel/Program.cs
+++ JsonToExcel/Program.cs
@@ -23,7 +23,7 @@ namespace JsonToExcel
 
                 var columns = new Dictionary<string, int>();
 
-                var json = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+                var json = Newtonsoft.Json.JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(jsonString);
                 foreach(var it in json)
                 {
                     var key = it.Key;
@@ -31,22 +31,29 @@ namespace JsonToExcel
                     var row = sheet.CreateRow(sheet.LastRowNum + 1);
                     row.CreateCell(0).SetCellValue(key);
 
-                    foreach(var pair in (Dictionary<string, object>)it.Value)
+                    foreach(var pair in (Newtonsoft.Json.Linq.JObject)it.Value)
                     {
                         var language = pair.Key;
                         var value = (string)pair.Value;
                         int column;
                         if (!columns.TryGetValue(language, out column))
                         {
-                            column = columns.Count + 2;
+                            column = columns.Count + 1;
                             columns.Add(language, column);
                         }
                         row.CreateCell(column).SetCellValue(value);
                     }
                 }
-
+                {
+                    var header = sheet.CreateRow(0);
+                    header.CreateCell(0).SetCellValue("key");
+                    foreach(var it in columns)
+                    {
+                        header.CreateCell(it.Value).SetCellValue(it.Key);
+                    }
+                }
                 Console.WriteLine("save " + excelPath);
-                using (var fs = new System.IO.FileStream(excelPath, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
+                using (var fs = new System.IO.FileStream(excelPath, System.IO.FileMode.Create, System.IO.FileAccess.Write))
                 {
                     book.Write(fs);
                 }
