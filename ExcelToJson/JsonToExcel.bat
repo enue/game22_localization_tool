864415539ecc13ae3df2b7c9e6cf83b6241a5cd9
enue <nnp@car.ocn.ne.jp>
2018-05-07 17:17:04 +0900

    exeの一元化

---
+++ ExcelToJson/JsonToExcel.bat
@@ -0,0 +1,11 @@
+"ExcelToJson.exe" reverse ^
+Localization.xlsx mst_develop_localize_Data/StreamingAssets/Localization.json ^
+LocalizationAbility.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationAbility.json ^
+LocalizationArticle.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationArticle.json ^
+LocalizationBattle.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationBattle.json ^
+LocalizationJob.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationJob.json ^
+LocalizationCharacter.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationCharacter.json ^
+LocalizationUnit.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationUnit.json ^
+LocalizationElement.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationElement.json ^
+LocalizationScript.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationScript.json ^
+LocalizationCredit.xlsx mst_develop_localize_Data/StreamingAssets/LocalizationCredit.json
