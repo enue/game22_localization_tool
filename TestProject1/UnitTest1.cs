1325133778e5c8a04a9c99f0e0afd1c7ebf3b5c0
enu <otoriden@gmail.com>
2021-06-20 19:26:33 +0900

    add test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    public class CellReferenceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(1u, 1u, "A1")]
+        [TestCase(26u, 1u, "Z1")]
+        [TestCase(27u, 1u, "AA1")]
+        [TestCase(28u, 1u, "AB1")]
+        [TestCase(26u * 2u, 1u, "AZ1")]
+        [TestCase(26u * 2u + 1, 1u, "BA1")]
+        [TestCase(26u * 3u, 1u, "BZ1")]
+        [TestCase(26u * 27u, 10u, "ZZ10")]
+        [TestCase(901u, 10u, "AHQ10")]
+        public void Test1(uint column, uint row, string text)
+        {
+            {
+                var pos = new TSKT.Bonn.CellReference(column, row);
+                Assert.AreEqual(text, pos.value);
+            }
+            {
+                var pos = new TSKT.Bonn.CellReference(text);
+                Assert.AreEqual(column, pos.columnIndex);
+                Assert.AreEqual(row, pos.rowIndex);
+            }
+        }
+
+        [Test]
+        public void Test2()
+        {
+            for (uint i = 1; i < 32768; ++i)
+            {
+                var pos = new TSKT.Bonn.CellReference(i, i);
+                var b = new TSKT.Bonn.CellReference(pos.value);
+                Assert.AreEqual(i, b.columnIndex);
+                Assert.AreEqual(i, b.rowIndex);
+            }
+        }
+    }
+}
